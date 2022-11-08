a4e5e013f460f44004fd8d6da8e2285281aa4265
ThayACarvalho <98364955+ThayACarvalho@users.noreply.github.com>
2022-11-07 21:21:10 -0300

    Add files via upload

---
+++ explorando dados de bike - junho.sql
@@ -0,0 +1,54 @@
+# Número total de viagens
+SELECT COUNT(ride_id) AS total_viagens FROM bikes.bikes_junho
+
+
+# Que tipo de bicicleta é mais usada?
+SELECT DISTINCT(COUNT(rideable_type)) AS Number_of_rides, rideable_type FROM bikes.bikes_junho
+GROUP BY rideable_type;
+
+# Existem mais ciclistas casuais ou membros?
+SELECT DISTINCT(COUNT(member_casual)) AS Number_of_members, member_casual FROM bikes.bikes_junho
+GROUP BY member_casual;
+
+# Média de minutos de viagem por tipo de cliente
+SELECT member_casual, AVG(Duration) AS average_duration_ride FROM bikes.bikes_junho 
+GROUP BY member_casual;
+
+# Qual tipo de bibicleta foi usada por períodos mais longos? 
+SELECT rideable_type, AVG(Duration) FROM bikes.bikes_junho
+GROUP BY rideable_type;
+
+# As 10 estações de início mais usadas no mês de junho
+SELECT start_station_name, COUNT(start_station_name) AS number_of_use_begining FROM bikes.bikes_junho
+WHERE start_station_name != ''
+GROUP BY start_station_name
+ORDER BY number_of_use_begining DESC LIMIT 10;
+
+# E as 10 estações de fim mais usadas
+SELECT end_station_name, COUNT(end_station_name) AS number_of_use FROM bikes.bikes_junho
+WHERE end_station_name != ''
+GROUP BY end_station_name
+ORDER BY number_of_use DESC LIMIT 10;
+
+# Quais dias da semana tem maior movimento?
+WITH viagens(data, viagens_dia, dia_semana) AS
+(
+SELECT DATE(started_at) AS data, COUNT(started_at) AS viagens_dia, DAYNAME(started_at) AS dia_semana FROM bikes.bikes_junho
+GROUP BY DAY(started_at)
+ORDER BY started_at)
+SELECT SUM(viagens_dia) AS numero_viagens, dia_semana FROM viagens
+GROUP BY dia_semana
+ORDER BY viagens_dia DESC
+
+# Qual o número de viagens com menos de 15 minutos?
+SELECT COUNT(duration) AS Viagens_menos_15min FROM bikes.bikes_junho
+WHERE duration < '00:15:00'
+
+# Viagens entre 15 e 30 minutos 
+SELECT COUNT(duration) AS Viagens_15_30min FROM bikes.bikes_junho
+WHERE duration BETWEEN '00:15:00' AND '00:30:00'
+
+
+# Viagens com mais de 30 minutos
+SELECT COUNT(duration) AS Viagens_mais_30min FROM bikes.bikes_junho
+WHERE duration > '00:30:00'
---
+++ explorando dados de bike - setembro.sql
@@ -0,0 +1,52 @@
+# Número total de viagens
+SELECT COUNT(ride_id) AS total_viagens FROM bikes.bikes_setembro
+
+
+# Que tipo de bicicleta é mais usada?
+SELECT DISTINCT(COUNT(rideable_type)) AS Number_of_rides, rideable_type FROM bikes.bikes_setembro
+GROUP BY rideable_type;
+
+# Existem mais ciclistas casuais ou membros?
+SELECT DISTINCT(COUNT(member_casual)) AS Number_of_members, member_casual FROM bikes.bikes_setembro
+GROUP BY member_casual;
+
+# Média de minutos de viagem por tipo de cliente
+SELECT member_casual, AVG(Duration) AS average_duration_ride FROM bikes.bikes_setembro 
+GROUP BY member_casual;
+
+# Qual tipo de bibicleta foi usada por períodos mais longos? 
+SELECT rideable_type, AVG(Duration) FROM bikes.bikes_setembro
+GROUP BY rideable_type;
+
+# As 10 estações de início mais usadas no mês de setembro
+SELECT start_station_name, COUNT(start_station_name) AS number_of_use_begining FROM bikes.bikes_setembro
+WHERE start_station_name != ''
+GROUP BY start_station_name
+ORDER BY number_of_use_begining DESC LIMIT 10;
+
+# E as 10 estações de fim mais usadas
+SELECT end_station_name, COUNT(end_station_name) AS number_of_use FROM bikes.bikes_setembro
+WHERE end_station_name != ''
+GROUP BY end_station_name
+ORDER BY number_of_use DESC LIMIT 10;
+
+# Quais dias da semana tem maior movimento?
+WITH viagens(data, viagens_dia, dia_semana) AS
+(
+SELECT DATE(started_at) AS data, COUNT(started_at) AS viagens_dia, DAYNAME(started_at) AS dia_semana FROM bikes.bikes_setembro
+GROUP BY DAY(started_at)
+ORDER BY started_at)
+SELECT data, viagens_dia, dia_semana FROM viagens
+ORDER BY viagens_dia DESC;
+
+# Qual o número de viagens com menos de 15 minutos?
+SELECT COUNT(duration) AS Viagens_menos_15min FROM bikes.bikes_setembro
+WHERE duration < '00:15:00'
+
+# Viagens entre 15 e 30 minutos 
+SELECT COUNT(duration) AS Viagens_menos_15min FROM bikes.bikes_setembro
+WHERE duration BETWEEN '00:15:00' AND '00:30:00'
+
+# Viagens com mais de 30 minutos
+SELECT COUNT(duration) AS Viagens_menos_15min FROM bikes.bikes_setembro
+WHERE duration > '00:30:00'
